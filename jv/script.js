ba34c5f86d42b8bc3dcde8e99731ab021c385176
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-15 15:45:15 +0200

    inner-card

--- jv/script.js
+++ jv/script.js
@@ -41,53 +41,18 @@ const teamMembers = [
 
 const cardContainer = document.getElementById("card-container");
 
-const card = `<div class="col-4">
-          <div class="card mb-3" style="max-width: 540px">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img
-                  src="./img/male1.png"
-                  class="img-fluid rounded-start"
-                  alt="..."
-                />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body text-bg-dark">
-                  <h5 class="card-title">nome e cognome</h5>
-                  <p class="card-text">designer</p>
-                  <p class="card-text">
-                    <small class="text-body-dark">email</small>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>`;
-let membersInfo;
+let card = ``;
 for (const Members of teamMembers) {
-  const name = name.value;
-  const role = role.value;
-  const email = email.value;
-  const img = img.value;
+  const { name, role, email, img } = Members;
 
-  const membersInfo = { name, role, email, img };
-
-  cardContainer.innerHTML(cardInfo);
-}
-
-console.log(cardContainer);
-
-// funzione per inner da inserire
-
-const cardInfo = () => {
-  const card = `<div class="col-4">
+  card += `<div class="col-4">
           <div class="card mb-3" style="max-width: 540px">
             <div class="row g-0">
               <div class="col-md-4">
                 <img
-                  src=".${img}"
+                  src="./${img}"
                   class="img-fluid rounded-start"
-                  alt="..."
+                  alt="${name}"
                 />
               </div>
               <div class="col-md-8">
@@ -102,8 +67,12 @@ const cardInfo = () => {
             </div>
           </div>
         </div>`;
+}
 
-  return card;
-};
+cardContainer.innerHTML = card;
+
+console.log(cardContainer);
+
+// funzione per inner da inserire
 
 console.log(card);
